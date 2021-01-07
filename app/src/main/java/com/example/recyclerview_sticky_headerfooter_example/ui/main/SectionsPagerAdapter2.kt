e92b7d80e55c44d1c192ff8035c5d502c5b104b1
danggai <donggi9313@naver.com>
2021-01-07 13:28:24 +0900

    - ViewPager replaced with ViewPager2

---
+++ app/src/main/java/com/example/recyclerview_sticky_headerfooter_example/ui/main/SectionsPagerAdapter2.kt
@@ -0,0 +1,67 @@
+package com.example.recyclerview_sticky_headerfooter_example.ui.main
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview_sticky_headerfooter_example.R
+import com.example.recyclerview_sticky_headerfooter_example.databinding.FragmentMainTabFirstBinding
+import com.example.recyclerview_sticky_headerfooter_example.databinding.FragmentMainTabSecondBinding
+import com.example.recyclerview_sticky_headerfooter_example.databinding.FragmentMainTabThirdBinding
+
+public class SectionsPagerAdapter2() : RecyclerView.Adapter<SectionsPagerAdapter2.ItemViewHolder>() {
+
+    companion object {
+        const val itemSize = 3
+
+        const val TYPE_HEADER_ONLY = 0
+        const val TYPE_FOOTER_ONLY = 1
+        const val TYPE_BOTH = 2
+    }
+
+    class ItemViewHolder(
+        layoutId: Int,
+        parent: ViewGroup,
+        val binding: ViewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), layoutId, parent, false)
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemViewHolder {
+        return when (viewType) {
+            TYPE_HEADER_ONLY -> {
+                ItemViewHolder(R.layout.fragment_main_tab_first, parent)
+            }
+            TYPE_FOOTER_ONLY -> {
+                ItemViewHolder(R.layout.fragment_main_tab_second, parent)
+            }
+            else -> {
+                ItemViewHolder(R.layout.fragment_main_tab_third, parent)
+            }
+        }
+    }
+
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+
+        when (holder.binding) {
+//            is FragmentMainTabFirstBinding -> {
+//                holder.binding.vm = viewModel
+//            }
+//            is FragmentMainTabSecondBinding -> {
+//                holder.binding.vm = viewModel
+//            }
+//            is FragmentMainTabThirdBinding -> {
+//                holder.binding.vm = viewModel
+//            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return itemSize
+    }
+
+
+}
